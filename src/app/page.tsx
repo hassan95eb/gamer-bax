b5d60694420971e803fed1e30f8ce203d46e1c87
hassan95eb <19h.amini95@gmail.com>
2026-01-28 19:27:32 +0330

    change some feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <div className="text-text p-2 pb-20 gap-16 sm:p-20">
-            این شروع ماجراست
-        </div>
-    );
+    return <Home />;
 }
